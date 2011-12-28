a30e94ad7689f554050ff9a15a98a2c8515ad203
herb143 <h.herbie@gmail.com>
2011-12-27 23:32:44 -0500

    Fixed issue with targeting players.

--- me/hgilman/Spells/SpellsInfoLabel.java
+++ me/hgilman/Spells/SpellsInfoLabel.java
@@ -2,6 +2,7 @@ package me.hgilman.Spells;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.getspout.spoutapi.gui.GenericLabel;
@@ -10,24 +11,32 @@ import org.getspout.spoutapi.player.SpoutPlayer;
 import org.getspout.spoutapi.inventory.SpoutItemStack;
 
 public class SpellsInfoLabel extends GenericLabel {
-	
+
 	private Player player;
 	private static Spells plugin;
-	
+
 	String printLabel(LivingEntity target)
 	{
 		String returnValue = "";
+
 		if (target==null)
 		{
 			returnValue = ChatColor.RED + "NOT SET" + ChatColor.WHITE;
 		}
 		else
 		{
-			returnValue = ChatColor.GREEN + target.toString().replaceFirst("Craft", "") + ChatColor.WHITE;
+			if(target instanceof CraftPlayer)
+			{
+				returnValue = ChatColor.GREEN + target.toString().replaceFirst("CraftPlayer\\{name=", "").replaceFirst("\\}", "") + ChatColor.WHITE;
+			}
+			else
+			{
+				returnValue = ChatColor.GREEN + target.toString().replaceFirst("Craft", "") + ChatColor.WHITE;
+			}
 		}
 		return returnValue;
 	}
-	
+
 	private String sClickToCast()
 	{
 		if(plugin.getPlayerData(player).isClickToCast())
@@ -39,8 +48,8 @@ public class SpellsInfoLabel extends GenericLabel {
 			return ChatColor.RED + "disabled" + ChatColor.WHITE;
 		}
 	}
-	
-	
+
+
 	SpellsInfoLabel(Spells iplugin, Player iplayer)
 	{
 		super("Loading info from Spells v2.0 database...");
@@ -48,15 +57,15 @@ public class SpellsInfoLabel extends GenericLabel {
 		player = iplayer;
 		this.setAlign(WidgetAnchor.BOTTOM_RIGHT); //Align the text against the top right corner of the Label
 		this.setAnchor(WidgetAnchor.BOTTOM_RIGHT); //Align the Label against the top right corner of the screen.
-		
+
 		this.setDirty(true);
 		((SpoutPlayer)player).getMainScreen().attachWidget(plugin,this);
 	}
-	
+
 	public void onTick()
 	{
 		SpellsPlayerData playerData = plugin.getPlayerData(player);
-		
+
 		LivingEntity target = playerData.getTarget();
 		if(playerData.getTarget() != null)
 		{
@@ -70,9 +79,9 @@ public class SpellsInfoLabel extends GenericLabel {
 			}
 		}
 
-		
+
 		this.setText("Target: " + printLabel(target) + "\nSpell: " + playerData.getSpellBook().getCurrentSpell().abilityFormat(ChatColor.RED,ChatColor.GREEN) + "\nClickToCast " + sClickToCast()).setDirty(true);
-		
+
 		if(new SpoutItemStack(player.getItemInHand()).isCustomItem())
 		{
 			this.setVisible(true).setDirty(true);
@@ -82,17 +91,17 @@ public class SpellsInfoLabel extends GenericLabel {
 			this.setVisible(false).setDirty(true);
 		}
 	}
-	
+
 	private double getDistance(Location locA, Location locB) // Our lovely distance formula.
 	{
-			double xdiff = locA.getX() - locB.getX();
-			double ydiff = locA.getZ() - locB.getZ();
-			double zdiff = locA.getY() - locB.getY();
-			double xdiffsq = xdiff * xdiff;
-			double ydiffsq = ydiff * ydiff;
-			double zdiffsq = zdiff * zdiff;
-			double xyzadd = xdiffsq + ydiffsq + zdiffsq;
-			return Math.sqrt(xyzadd);
+		double xdiff = locA.getX() - locB.getX();
+		double ydiff = locA.getZ() - locB.getZ();
+		double zdiff = locA.getY() - locB.getY();
+		double xdiffsq = xdiff * xdiff;
+		double ydiffsq = ydiff * ydiff;
+		double zdiffsq = zdiff * zdiff;
+		double xyzadd = xdiffsq + ydiffsq + zdiffsq;
+		return Math.sqrt(xyzadd);
 	}
-	
+
 }
